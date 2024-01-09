d29a9a250f34ac02fba5eaa3924492d93434244b
lynn <cedriccolinc@yahoo.com>
2024-01-09 10:29:02 +0100

    Structure

---
+++ lp_filter/LPFilter1.java
@@ -0,0 +1,14 @@
+public class LPFilter1 {
+    public double[] lpFilter(double[] inputSignal, double sampleFreq, double cutoffFreq) {
+        double rc = 1.0 / (cutoffFreq * 2 * Math.PI);
+        double dt = 1.0 / sampleFreq;
+        double alpha = dt / (rc + dt);
+        double[] output = new double[inputSignal.length];
+
+        output[0] = inputSignal[0];
+        for (int i = 1; i < inputSignal.length; i++) {
+            output[i] = alpha * inputSignal[i] + (1 - alpha) * output[i - 1];
+        }
+        return output;
+    }
+}
